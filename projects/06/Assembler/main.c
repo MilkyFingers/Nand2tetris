d9837a5541e2698abd28d7cce1e670a3c3c4eccf
Micah <micahbassett9@gmail.com>
2023-10-02 19:37:56 +0100

    correct files

--- projects/06/Assembler/main.c
+++ projects/06/Assembler/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include "parser.h"
 
 int main(int argc, char* argv[])
 {
     FILE *fp;
+    /*
+    This file path is used on my mac machine 
     fp = fopen("/Users/micahbassett/Desktop/Nand2tetris/projects/06/add/Add.asm", "r");
+    */
+
+   fp = fopen("C:/Users/micah/OneDrive/Desktop/Nand2tetris/projects/06/add/Add.asm", "r");
 
     char line[100];
     fpos_t position;
 
-    fgetpos(fp, &position);
-
-    for (int i=0; i<50; i++) {
-        fsetpos(fp, &position);
-        fgets(line, 60, fp);
-        fgetpos(fp, &position);
+    while (!feof(fp))
+    {
+        advance(fp, position, line);
         printf(line);
     }
 }
